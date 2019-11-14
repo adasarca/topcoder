78c3bf3cb55dd030ae39cc156fec4d3df72a8bb8
Ada <adela.sarca@staff.bluematrix.com>
2019-11-14 14:07:19 +0200

    CostOfDancing

---
+++ src/main/java/com/example/topcoder/CostOfDancing.java
@@ -0,0 +1,56 @@
+/*
+ * Created by AdelaSarca
+ * Date: 11/14/2019
+ */
+package com.example.topcoder;
+
+public class CostOfDancing {
+
+    public int minimum(int k, int[] danceCost) {
+        this.sort(danceCost, 0, danceCost.length - 1);
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += danceCost[i];
+        }
+
+        return sum;
+    }
+
+    private void sort(int[] a, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int m = (l + r) / 2;
+
+        this.sort(a, l, m);
+        this.sort(a, m + 1, r);
+        this.merge(a, l, m, r);
+    }
+
+    private void merge(int[] a, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int a1[] = new int[n1];
+        int a2[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            a1[i] = a[l + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            a2[i] = a[m + i + 1];
+        }
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            a[k++] = a1[i] < a2[j] ? a1[i++] : a2[j++];
+        }
+        while (i < n1) {
+            a[k++] = a1[i++];
+        }
+        while (j < n2) {
+            a[k++] = a2[j++];
+        }
+    }
+}
---
+++ src/test/java/com/example/topcoder/CostOfDancingTest.java
@@ -0,0 +1,86 @@
+/*
+ * Created by AdelaSarca
+ * Date: 11/14/2019
+ */
+package com.example.topcoder;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CostOfDancingTest {
+
+    private CostOfDancing costOfDancing;
+
+    @BeforeMethod
+    public void setup() {
+        this.costOfDancing = new CostOfDancing();
+    }
+
+    @Test
+    public void testMinimum_Example0() {
+        //setup
+        int k = 2;
+        int[] danceCost = new int[] {1, 5, 3, 4};
+
+        //test
+        int cost = this.costOfDancing.minimum(k, danceCost);
+
+        //verify
+        assertEquals(cost, 4);
+    }
+
+    @Test
+    public void testMinimum_Example1() {
+        //setup
+        int k = 3;
+        int[] danceCost = new int[] {1, 5, 4};
+
+        //test
+        int cost = this.costOfDancing.minimum(k, danceCost);
+
+        //verify
+        assertEquals(cost, 10);
+    }
+
+    @Test
+    public void testMinimum_Example2() {
+        //setup
+        int k = 1;
+        int[] danceCost = new int[] {2, 2, 4, 5, 3};
+
+        //test
+        int cost = this.costOfDancing.minimum(k, danceCost);
+
+        //verify
+        assertEquals(cost, 2);
+    }
+
+    @Test
+    public void testMinimum_Example3() {
+        //setup
+        int k = 39;
+        int[] danceCost = new int[] {973, 793, 722, 573, 521, 568, 845, 674, 595, 310, 284, 794, 913, 93, 129, 758, 108, 433, 181, 163, 96, 932,
+                703, 989, 884, 420, 615, 991, 364, 657, 421, 336, 801, 142, 908, 321, 709, 752, 346, 656, 413, 629, 801};
+
+        //test
+        int cost = this.costOfDancing.minimum(k, danceCost);
+
+        //verify
+        assertEquals(cost, 20431);
+    }
+
+    @Test
+    public void testMinimum_OneCourse() {
+        //setup
+        int k = 1;
+        int[] danceCost = new int[] {5};
+
+        //test
+        int cost = this.costOfDancing.minimum(k, danceCost);
+
+        //verify
+        assertEquals(cost, 5);
+    }
+}
