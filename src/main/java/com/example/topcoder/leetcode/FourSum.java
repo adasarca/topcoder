b613d4cd458c91bbed002977851d955a57e80935
ada <adasarca@gmail.com>
2020-03-22 15:48:09 +0200

    leetcode // FourSum

---
+++ src/main/java/com/example/topcoder/leetcode/FourSum.java
@@ -0,0 +1,62 @@
+package com.example.topcoder.leetcode;
+
+import java.util.*;
+
+public class FourSum {
+    private HashMap<Integer, Integer> map;
+    private ArrayList<Integer> numbers;
+    private int target;
+
+    private Integer[] comb;
+    private List<List<Integer>> output;
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        if (n < 4) return Collections.emptyList();
+
+        this.map = new HashMap<>();
+        this.numbers = new ArrayList<>();
+        for (int x : nums) {
+            Integer count = this.map.get(x);
+            if (count == null) {
+                count = 0;
+                this.numbers.add(x);
+            }
+            this.map.put(x, count + 1);
+        }
+        Collections.sort(this.numbers);
+        this.target = target;
+
+        this.comb = new Integer[4];
+        this.output = new LinkedList<>();
+        this.generate(0, 0, 0);
+        return this.output;
+    }
+
+    private void generate(int i, int j, int sum) {
+        if (i == 3) {
+            int x = this.target - sum;
+            if (x >= this.comb[i - 1] && this.map.getOrDefault(x, 0) > 0) {
+                this.comb[i] = x;
+                List<Integer> list = new LinkedList<>();
+                Collections.addAll(list, this.comb);
+                this.output.add(list);
+            }
+            return;
+        }
+
+        int m = this.numbers.size();
+        for (int k = j; k < m; k++) {
+            Integer x = this.numbers.get(k);
+            this.comb[i] = x;
+
+            Integer count = this.map.get(x);
+            int nextJ = count > 1 ? k : k + 1;
+            this.map.put(x, count - 1);
+
+            this.generate(i + 1, nextJ, sum + x);
+
+            this.map.put(x, count);
+        }
+    }
+}
---
+++ src/test/java/com/example/topcoder/leetcode/FourSumTest.java
@@ -0,0 +1,37 @@
+package com.example.topcoder.leetcode;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class FourSumTest {
+
+    private FourSum fourSum;
+
+    @BeforeMethod
+    public void setup() {
+        this.fourSum = new FourSum();
+    }
+
+    @Test
+    public void testFourSum() throws Exception {
+        //setup
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(-2, -1, 1, 2),
+                Arrays.asList(-2,  0, 0, 2),
+                Arrays.asList(-1,  0, 0, 1)
+        );
+
+        //test
+        List<List<Integer>> comb = this.fourSum.fourSum(nums, 0);
+
+        //verify
+        assertEquals(comb, expected);
+    }
+
+}
